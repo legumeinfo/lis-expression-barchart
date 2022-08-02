a3abb16066d0d633ed63cddb0587cd5f76005345
Sam Hokin <shokin@ncgr.org>
2022-08-02 13:57:05 -0600

    SequenceFeature -> Gene and legumeinfo/ on package name

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import RootContainer from './RootContainer';
 
 // make sure to export main, with the signature
 function main(el, service, imEntity, state, config) {
-	state = state || {};
+	if (!state) state = {};
 	if (!el || !service || !imEntity || !state || !config) {
 		throw new Error('Call main with correct signature');
 	}
 	ReactDOM.render(
 		<RootContainer
 			serviceUrl={service.root}
-			entity={imEntity.SequenceFeature}
+			entity={imEntity.Gene}
 			config={config}
 		/>,
 		el
